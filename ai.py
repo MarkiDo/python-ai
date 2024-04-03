6dd71a692dda6b856a0440c21c7a4219b0f7b783
MarkiDo <portaldn@gmail.com>
2024-04-04 00:49:49 +0300

    new testing files added

---
+++ ai.py
@@ -0,0 +1,33 @@
+import os
+import re
+from openai import OpenAI
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+api_key = os.environ.get('API_KEY')
+
+GREEN = '\033[92m'
+RED = '\033[91m'
+RESET = '\033[0m'
+
+client = OpenAI(api_key=api_key)
+
+def comp(PROMPT, MaxToken=50):
+    pattern = r'[!@#$%^&*(),.?":{}|<>]'
+    if not re.search(pattern,PROMPT):
+        response = client.chat.completions.create(
+            model="gpt-3.5-turbo",
+              messages=[{"role": "user", "content": PROMPT }],
+            max_tokens=MaxToken,
+            temperature=0
+        )
+        return response.choices[0].message.content
+    else:
+        return print(RED + 'Incorrect input'+ RESET)
+
+PROMPT = input('Ask ChatGPT a question: ')
+
+response= comp(PROMPT, MaxToken=3000)
+print(GREEN+response+RESET)
---
+++ main.py
@@ -0,0 +1,5 @@
+name = input('Input name:')
+# print(name)
+RED = '\033[91m'
+RESET = '\033[0m'
+print(RED + "This is red text." + RESET)
